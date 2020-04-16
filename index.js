be11bd5fc001898d7337f9edf4dd0d33d2803271
Agustinus Adam <agustinusadamw@gmail.com>
2020-04-16 23:47:15 +0700

    promise-multiple-file

--- index.js
+++ index.js
@@ -9,13 +9,52 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+  return new Promise((res, rej)=> {
+    fs.readFile(path, 'utf8', (err, data) => {
+      sleep(2000)
+      if (err) {
+        rej(err)
+      } else {
+        res(JSON.parse(data))
+      }
+    })
+  })
 }
+
+
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(parents_data => {
+    return readFilePromise(childrenFileName)
+    .then(childsdata => {
+      for (let i = 0; i < parents_data.length; i++) {
+        parents_data[i].children = []
+      }
+      for (let i = 0; i < parents_data.length; i++) {
+        for (let j = 0; j < childsdata.length; j++) {
+          if (parents_data[i].last_name === childsdata[j].family) {
+            parents_data[i].children.push(childsdata[j].full_name)
+          }
+        }
+      }
+      console.log(parents_data)
+    })
+    .catch(error => {
+      console.log('Ini Error 2 = \n',  error);
+    })
+  })
+  .catch(error => {
+    console.log('Ini Error 1 = \n',  error);
+  })
+  
+  
+  
 }
+
 
 matchParentsWithChildren('./parents.json', './children.json');
 console.log("Notification : Data sedang diproses !");
