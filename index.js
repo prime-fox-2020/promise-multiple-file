2fb96e3cf78db3af8dba5dcc8712545013d7c7c6
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-04-17 03:10:05 +0700

    promise multiple file done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+let parent;
+// let child;
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -9,11 +11,38 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(path) {
+  return new Promise((res, rej)=>{
+    fs.readFile(path, 'utf8', (err, data)=>{
+      if(err) rej(err)
+
+      else
+      res(JSON.parse(data))
+    })
+  })
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
+  readFilePromise(parentFileName)
+  .then( data=>{
+    parent = data
+    return readFilePromise(childrenFileName)
+  })
+  .then(data2 =>{
+    sleep(1000)
+    for(let a = 0; a < parent.length; a++){
+      parent[a].children = []
+      for(let b = 0; b < data2.length; b++){
+        if(parent[a].last_name == data2[b].family){
+          parent[a].children.push(data2[b].full_name)
+        }
+      }
+    }
+    console.table(parent)
+  })
+  .catch(err =>{
+    console.log('sumtin wong')
+  })
   // your code here... (p.s. readFilePromise function(s) should be around here..)
 }
 
