bac6bf421f3100ef3469d8e7a2d9706647329478
zuhdi <zudhy.duddy@gmail.com>
2020-04-16 23:55:11 +0700

    coba tanpa promisify

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function readFilePromise(path) {
       if(err){
         rej(err)
       }else{
+        sleep(5000)
         res(JSON.parse(data))
       }
     })
@@ -25,15 +26,11 @@ function matchParentsWithChildren(parentFileName, childrenFileName) {
   let parent_data;
   readFilePromise(parentFileName)
   .then(dataParents=>{
-    sleep(5000)
     parent_data = dataParents
-    // console.log(parent_data);
+
     return readFilePromise(childrenFileName)
   })
   .then(dataChildrens=>{
-    sleep(5000)
-    // dataChildrens
-    // console.log(parent_data);
     for (let i = 0; i < parent_data.length; i++) {
       parent_data[i].children = []
       for (let j = 0; j < dataChildrens.length; j++) {
@@ -48,6 +45,40 @@ function matchParentsWithChildren(parentFileName, childrenFileName) {
     console.log(err);
   })
 }
+
+// Tanpa promisify
+
+/*
+function matchParentsWithChildren(parentFileName, childrenFileName){
+  let parent_data;
+
+  fs.promises.readFile(parentFileName, 'utf8')
+  .then(dataParents=>{
+    sleep(5000)
+    parent_data = JSON.parse(dataParents)
+
+    return fs.promises.readFile(childrenFileName, 'utf8')
+  })
+  .then(dataChildrens=>{
+    sleep(5000)
+    childrens = JSON.parse(dataChildrens)
+    
+    for (let i = 0; i < parent_data.length; i++) {
+      parent_data[i].children = []
+      for (let j = 0; j < childrens.length; j++) {
+        if(parent_data[i].last_name === childrens[j].family){
+        parent_data[i].children.push(childrens[j].full_name)
+        }
+      }
+    }
+    console.log(parent_data);
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+}
+*/
+
 
 matchParentsWithChildren('./parents.json', './children.json');
 console.log("Notification : Data sedang diproses !");
