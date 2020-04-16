0e8968d5dd0b57074e0ceccf6d45098c75e3fa8e
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-04-17 01:46:07 +0700

    tugas promise multifile

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const promises = fs.promises;
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -9,12 +10,42 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+  return new Promise ((res, rej) => {
+    promises.readFile(path, 'utf8')
+    .then (data => {
+      res(JSON.parse(data))
+    })
+    .catch (err => {
+      rej(err)
+    })
+  })
 }
 
+
+let result;
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(parentsData => {
+    result = parentsData
+    return readFilePromise(childrenFileName)
+  })
+  .then (childrenData => {
+    for (let i = 0; i < result.length; i++) {
+      result[i].children = []
+      for (let j = 0; j < childrenData.length; j++) {
+        if (result[i].last_name == childrenData[j].family) {
+          result[i].children.push(childrenData[j].full_name)
+        }
+      }
+    }
+    console.log(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
