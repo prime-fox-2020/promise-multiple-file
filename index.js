c620660fba65e41bde6ef84ae90cee76dfebf818
Adib Faqih <foezst@gmail.com>
2020-04-17 03:09:22 +0700

    promise multiple file

--- index.js
+++ index.js
@@ -9,12 +9,40 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(fileName) {
+  return new Promise((res, rej) => {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(data);
+      }
+    })
+  })
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let parent, children;
+  readFilePromise(parentFileName)
+    .then(data => {
+      parent = JSON.parse(data);
+      sleep(5000);
+      return readFilePromise(childrenFileName);
+    })
+    .then(data => {
+      children = JSON.parse(data);
+      sleep(5000);
+      for (let i = 0; i < parent.length; i++) {
+        parent[i].children = [];
+        for (let j = 0; j < children.length; j++) {
+          if (parent[i].last_name === children[j].family) {
+            parent[i].children.push(children[j].full_name);
+          }
+        }
+      }
+      console.log(parent);
+    })
+    .catch(err => console.log(err));
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
