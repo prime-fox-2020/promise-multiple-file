53a6acec89f3df82af601f91fd53e8a2f02a1a6e
michaelm96 <michaelarceus96@gmail.com>
2020-04-17 00:05:51 +0700

    update promise multiple-file

--- index.js
+++ index.js
@@ -9,12 +9,50 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+
+  return new Promise((res, rej) => {
+    fs.readFile(path, 'utf8', (err,data) => {
+      if(err){
+        rej(err)
+      }else{
+        res(JSON.parse(data))
+        sleep(1000)
+      }
+    })
+  })
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+
+  let parent_Data = []
+  let children_Data = []
+  
+  readFilePromise(parentFileName)
+  .then( data1 => {
+    parent_Data = data1
+    return readFilePromise(childrenFileName)
+  })
+  .then( data2 => {
+    children_Data = data2
+    // console.log(parent_Data)
+    // console.log(children_Data)
+    for (let i = 0; i < parent_Data.length; i++) {
+      parent_Data[i].family = []
+      for (let j = 0; j < children_Data.length; j++) {
+        if (parent_Data[i].last_name == children_Data[j].family) {
+          parent_Data[i].family.push(children_Data[j].full_name)
+        }
+      }
+    }
+    console.log(parent_Data)
+  })
+  .catch( error => {
+    console.log(`Error nih karena :\n`, error);
+    console.log("-------------------------------------------------");
+  })
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
