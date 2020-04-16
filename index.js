661bedd5649ae70988ab187a88e5106a13dc704d
Sujackson <su_jackson99@yahoo.com>
2020-04-16 21:57:10 +0700

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -9,12 +9,45 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+
+
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise((res,rej)=>{
+    fsPromises.readFile(file,'utf-8')
+    .then(result=>{
+      res(JSON.parse(result))
+    })
+    .catch(err=>{
+      rej(err)
+    })
+  })
+ 
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(data=>{
+      readFilePromise(childrenFileName)
+      .then(dataAnak=>{
+        for (let i = 0; i < data.length; i++) {
+          data[i].children=[]
+          for (let j = 0; j < dataAnak.length; j++) {
+            if(data[i].last_name==dataAnak[j].family){
+              data[i].children.push(dataAnak[j].full_name)
+            }
+          }
+        }
+        console.log(data)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+  })
+  .catch(err=>{
+    console.log(err)
+  })
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
