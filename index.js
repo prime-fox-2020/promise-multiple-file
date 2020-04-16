95923d172d5010ad1b6d9f9f165c97c788dc2cba
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-04-16 23:25:29 +0700

    finished index.js

--- index.js
+++ index.js
@@ -9,12 +9,40 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+  return new Promise ((res, rej) => {
+    fs.readFile(path,'utf8', (err, data)=> {
+    if(err) rej(err)
+    else res(JSON.parse(data))
+    })
+  })
 }
 
+let result;
+
 function matchParentsWithChildren(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+// your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise('./parents.json')
+  .then( data1 => {
+    result = data1;
+    return readFilePromise('./children.json')
+  })
+  // sleep(5000);
+  .then( data2 => {
+    for (let i = 0; i < result.length; i++) {
+      result[i].children = [];
+      for (let j = 0; j < data2.length; j++) {
+        if (result[i].last_name === data2[j].family) {
+          result[i].children.push(data2[j].full_name);
+        }
+      }
+    }
+    console.log(result);
+  })
+  .catch((err)=> {
+    console.log(err);
+  })
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
