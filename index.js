afaa1dfa7e6cb9bffccc0df19792cf78faeed018
pippop21 <afifar38@gmail.com>
2020-04-17 07:49:44 +0700

    done

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+  return new Promise((res, rej) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if(err) rej(err)
+      else res (JSON.parse(data))
+    })
+  })
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let parents = []
+  readFilePromise(parentFileName)
+  .then(parentName => {
+    parents = parentName
+    console.log(parentName)
+    return readFilePromise(childrenFileName)
+  })
+  .then(childrenName => {
+    console.log(childrenName)
+    for(let parent of parents){
+      parent.children = [];
+      for(let children of childrenName){
+        if(children.family === parent.last_name) parent.children.push(children.full_name);
+      }
+    }
+    sleep(3000)
+    console.log(parents)
+  })
+  .catch(err => console.log(err))
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
