289ccf03b5a9777ff1bd9d496bee9dddd5d9aece
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-04-17 08:40:49 +0700

    airell

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
 const fs = require('fs');
 
 function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if (new Date().getTime() - start > milliseconds) {
+			break;
+		}
+	}
 }
 
 function readFilePromise(path1) {
-  // psst, the promise should be around here...
-  return new Promise((res,rej)=>{
-    fs.readFile(path1, 'utf8', (err1,data1)=>{
-      if(err1){
-        rej(err1)
-      }else{
-        res(JSON.parse(data1))
-        
-       
-        
-      }
-      
-    })
-  })
-
+	// psst, the promise should be around here...
+	return new Promise((res, rej) => {
+		fs.readFile(path1, 'utf8', (err1, data1) => {
+			if (err1) {
+				rej(err1);
+			} else {
+				res(JSON.parse(data1));
+			}
+		});
+	});
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
-let kotak = []
- readFilePromise(parentFileName)
-.then(data1=>{
- for(var i = 0; i < data1.length; i++){
-  kotak.push(data1[i])
- }
+	// your code here... (p.s. readFilePromise function(s) should be around here..)
+	let kotak = [];
+	readFilePromise(parentFileName)
+		.then((data1) => {
+			for (var i = 0; i < data1.length; i++) {
+				kotak.push(data1[i]);
+			}
 
-  return readFilePromise(childrenFileName)
-})
-.then(anak=>{
-  for(var i = 0; i < kotak.length; i++){
-    kotak[i].children = [];
-    for(var j = 0; j < anak.length; j++){
-      if(kotak[i].last_name === anak[j].family){
-        kotak[i].children.push(anak[j].full_name)
-      }
-    }
-   
-  }
-  console.log(kotak)
-})
-.catch(error1=>{
-  console.log(error1)
- })
-
+			return readFilePromise(childrenFileName);
+		})
+		.then((anak) => {
+			for (var i = 0; i < kotak.length; i++) {
+				kotak[i].children = [];
+				for (var j = 0; j < anak.length; j++) {
+					if (kotak[i].last_name === anak[j].family) {
+						kotak[i].children.push(anak[j].full_name);
+					}
+				}
+			}
+			console.log(kotak);
+		})
+		.catch((error1) => {
+			console.log(error1);
+		});
 }
 
 // matchParentsWithChildren('./parents.json', './children.json');
