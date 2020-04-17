edf41ea333f87e406a005db236769615ee200a5c
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-17 08:31:37 +0700

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsPromises = fs.promises
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -9,12 +10,39 @@ function sleep(milliseconds) {
   }
 }
 
-function readFilePromise() {
+function readFilePromise(path) {
   // psst, the promise should be around here...
+  return new Promise ((reso, reje) => {
+    fsPromises.readFile(path, 'utf8')
+    .then(data => reso(JSON.parse(data)))
+    .catch( err => reje(err))
+  })
 }
 
 function matchParentsWithChildren(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let buffer
+  readFilePromise(parentFileName)
+    .then(data => {
+      buffer = data
+      return readFilePromise(childrenFileName)
+    })
+    .then(data => {
+      for(let i = 0; i < buffer.length; i++){
+        for(let j = 0; j < data.length; j++){
+          if(data[j].family === buffer[i].last_name){
+            if(buffer[i].children === undefined){
+              buffer[i].children = []
+              buffer[i].children.push(data[j].full_name)
+            }else{
+              buffer[i].children.push(data[j].full_name)
+            }
+          }
+        }
+      }
+      console.log(buffer)
+    })
+    .catch(err => {console.log(err)})
 }
 
 matchParentsWithChildren('./parents.json', './children.json');
